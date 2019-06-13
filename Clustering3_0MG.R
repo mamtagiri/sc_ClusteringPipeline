af7f9dfe3027e997e9bc0dc224f9f1ab1dcb00f8
mamtagiri <44902002+mamtagiri@users.noreply.github.com>
2019-06-13 15:50:04 -0400

    Update Clustering3_0MG.R

--- Clustering3_0MG.R
+++ Clustering3_0MG.R
@@ -2,7 +2,7 @@
 #     define functions          #
 #################################
 ##!/bin/Rscript
-
+args = commandArgs(trailingOnly=TRUE)
 #install.packages("Seurat","dplyr","cowplot")
 
 args = commandArgs(trailingOnly=TRUE)
